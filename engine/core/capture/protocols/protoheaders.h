8f92ddad3669fbf9e622440efcdd5745a798fe60
aminexd33 <amine.gamer222@gmail.com>
2025-12-17 14:01:37 +0100

    IPV6 header structure

--- engine/core/capture/protocols/protoheaders.h
+++ engine/core/capture/protocols/protoheaders.h
@@ -9,6 +9,15 @@ typedef struct{
     u_int16_t tci;// PCP(3 bit) | DEI(1 bit) | VID (12 bits)
 }vlan_tci;
 
+typedef struct{
+    // version 4bits priority 8bits 20bits flow
+    u_int32_t ver_prio_flow;
+    u_int32_t lenght_nheader_hoplimit;
+    u_int64_t source_address[2];
+    u_int64_t destin_address[2];
+}IPV6;
+
+
 void protocol_mapper(struct ip *iph);
 
 
